559a780946d2e99ca59dca76cbdb2c8abb3331a0
Miriam <118216207+cotufetes@users.noreply.github.com>
2023-02-22 09:55:56 +0100

    added comments to pokemonList array

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
 let pokemonList = [];
 
 pokemonList = [
-  {
+  {//Pokemon 0
   name: 'Bulbasaur',
   height: 0.7,
   type: ['grass', 'poison']
   },
-  {
+  {//Pokemon 1
   name: 'Ivysaur',
   height: 1,
   type: ['grass','poison']
   },
-  {
+  {//Pokemon 3
   name: 'Venusaur',
   height: 2,
   type: ['grass','poison']
