ca3c05eaba1fd283f664f1961e6b2d33ce98467b
Miriam <118216207+cotufetes@users.noreply.github.com>
2023-02-22 09:47:16 +0100

    declared pokemonList js variable

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+let pokemonList = [];
+
+pokemonList = [
+  {
+  name: 'Bulbasaur',
+  height: 0.7,
+  type: ['grass', 'poison']
+  },
+  {
+  name: 'Ivysaur',
+  height: 1,
+  type: ['grass','poison']
+  },
+  {
+  name: 'Venusaur',
+  height: 2,
+  type: ['grass','poison']
+  }
+];
